e4a3993e31aa02b9f4d17eebf01aba63b67ee951
xini <timcook751@yaoo.com>
2022-03-16 18:27:18 +0800

    editor create

---
+++ src/iviews/Editor.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>123</div>
+</template>
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
