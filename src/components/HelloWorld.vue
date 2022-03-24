553923ab5a9ddfdf8c50384ad246d3019494d173
xini <timcook751@yaoo.com>
2022-03-24 10:35:34 +0800

    t

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>123</h1>
+  {{ msg }}
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props:{ 
+    msg: String
+  },
+  setup() {
+    const msg1 = "a";
+    return {
+      msg1,
+    };
+  },
+});
+</script>
